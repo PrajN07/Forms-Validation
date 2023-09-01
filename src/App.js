8030c6585d8ada392f88a477c40f3bdebe3a05fb
PrajN07 <pn8296@srmist.edu.in>
2023-09-02 00:04:39 +0530

    changes in app component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import BasicForm from "./components/BasicForm";
-
+import SimpleInput from "./components/SimpleInput";
 
 
 function App() {
   return (
     <div className="app">
-     
+     <p>Hello world</p>
      <BasicForm/>
+     <SimpleInput/>
       
     </div>
   );
